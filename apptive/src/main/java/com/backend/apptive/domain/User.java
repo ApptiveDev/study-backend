5fae37a2c21d26d834b5b8ee8ea3ff3e3b1b8859
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-05-10 22:58:15 +0900

    feat:user 엔티티 추가

--- apptive/src/main/java/com/backend/apptive/domain/User.java
+++ apptive/src/main/java/com/backend/apptive/domain/User.java
@@ -1,4 +1,24 @@
 package com.backend.apptive.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+    @Id
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
 }
