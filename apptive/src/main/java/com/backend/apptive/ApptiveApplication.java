d5eb0406de5232d4bc924053ff811600f02b8602
wvssm <neoramiregi@gmail.com>
2024-05-14 16:03:51 +0900

    fix: application.yml DB 설정 변경

--- apptive/src/main/java/com/backend/apptive/ApptiveApplication.java
+++ apptive/src/main/java/com/backend/apptive/ApptiveApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApptiveApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApptiveApplication.class, args);
 	}
