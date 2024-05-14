d85a275ad2591f0bfe660a9fa76b18b4a137b569
wvssm <neoramiregi@gmail.com>
2024-05-14 14:03:30 +0900

    feat: User Entity 생성

--- apptive/src/main/java/com/backend/apptive/domain/User.java
+++ apptive/src/main/java/com/backend/apptive/domain/User.java
@@ -1,4 +1,30 @@
 package com.backend.apptive.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class User {
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Id
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Builder
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void update(String name) {
+        this.name = name;
+    }
 }
