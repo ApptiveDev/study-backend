84aed15eb36586fd7b6d8e01c58334bf82e71b00
wvssm <neoramiregi@gmail.com>
2024-11-24 10:27:30 +0900

    feat: 상품 관련 에러코드 추가

--- apptive/src/main/java/apptive/study/exception/ErrorCode.java
+++ apptive/src/main/java/apptive/study/exception/ErrorCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     MEMBER_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
-    MEMBER_NAME_DUPLICATE_ERROR(HttpStatus.BAD_REQUEST, "회원의 이름은 중복될 수 없습니다.");
+    MEMBER_NAME_DUPLICATE_ERROR(HttpStatus.BAD_REQUEST, "회원의 이름은 중복될 수 없습니다."),
+    PRODUCT_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다."),
+    PRODUCT_NAME_DUPLICATE_ERROR(HttpStatus.BAD_REQUEST, "상품의 이름은 중복될 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
