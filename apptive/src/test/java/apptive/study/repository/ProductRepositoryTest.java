a7f3a256f1e2eac98ae41f943ac8e9b586755138
wvssm <neoramiregi@gmail.com>
2024-11-24 10:32:50 +0900

    feat: 상품 레퍼지토리 테스트 작성

---
+++ apptive/src/test/java/apptive/study/repository/ProductRepositoryTest.java
@@ -0,0 +1,27 @@
+package apptive.study.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class ProductRepositoryTest {
+
+    @Test
+    @DisplayName("상품 등록 테스트")
+    void 상품_등록_테스트(){
+
+    }
+
+    @Test
+    @DisplayName("이름으로 상품 조회 테스트")
+    void 이름으로_상품조회_테스트() {
+
+    }
+
+    @Test
+    @DisplayName("전체 조회 테스트")
+    void 전체_조회_테스트() {
+
+    }
+}
