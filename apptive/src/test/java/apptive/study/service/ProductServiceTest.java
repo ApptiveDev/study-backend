2f24b2022516519765c9312d6041fd38ab0f830e
wvssm <neoramiregi@gmail.com>
2024-11-24 10:32:39 +0900

    feat: 상품 서비스 테스트 작성

---
+++ apptive/src/test/java/apptive/study/service/ProductServiceTest.java
@@ -0,0 +1,38 @@
+package apptive.study.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductServiceTest {
+
+    @Test
+    @DisplayName("상품 정상 등록 테스트")
+    void 상품_정상_등록_테스트() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    @DisplayName("중복된 이름의 상품 등록 테스트 - 예외 발생")
+    void 중복된_상품_이름_등록_테스트(){
+
+    }
+
+    @Test
+    @DisplayName("이름으로 상품 조회 테스트 - 상품이 존재할 때")
+    void 이름으로_상품조회_상품이_존재할_때() {
+    }
+
+    @Test
+    @DisplayName("이름으로 상품 조회 테스트 - 상품이 존재하지 않을 때")
+    void 이름으로_상품조회_상품이_존재하지_않을_때() {
+    }
+
+    @Test
+    @DisplayName("전체 상품 조회")
+    void findAll() {
+    }
+}
